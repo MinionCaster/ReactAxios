4b2d6836ae3957d335b13da7f5cae0db882d9f4e
Psych00l <35641863+Psych00l@users.noreply.github.com>
2018-07-24 15:58:57 +0200

    added delete

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -7,6 +7,7 @@ class FullPost extends Component {
     state = {
         loadedPost: null
     }
+
     async componentDidUpdate() {
         if (this.props.id) {
             if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
@@ -15,6 +16,14 @@ class FullPost extends Component {
             }
         }    
     }
+
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+            });
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if (this.props.id) {
@@ -26,7 +35,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
                     </div>
                 </div>
 
